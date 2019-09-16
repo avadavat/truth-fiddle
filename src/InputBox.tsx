5e3add73616d3382547923ab1550681695f07891
Daniel Gil <dangil@microsoft.com>
2019-09-16 08:05:54 -0700

    Add input box styling

--- src/InputBox.tsx
+++ src/InputBox.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
+const inputBoxStyle: React.CSSProperties = {
+  borderRadius: 24,
+  paddingLeft: 8,
+  paddingRight: 8,
+  fontSize: 24,
+};
+
 export const InputBox = React.memo(function InputBox() {
-  return <input />;
+  return <input style={inputBoxStyle} />;
 });
