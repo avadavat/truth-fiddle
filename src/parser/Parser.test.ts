46cfefc87948db1c91f72aa5f2660ec5ea17512f
Daniel Gil <dangil@microsoft.com>
2019-09-16 17:17:24 -0700

    Add initial parser tests

---
+++ src/parser/Parser.test.ts
@@ -0,0 +1,25 @@
+import { isValid } from '.';
+
+describe('simple expression tests', () => {
+  it('evaluates "p AND q" to be valid', () => {
+    expect(isValid('p AND q')).toBe(true);
+  });
+
+  it('evaluates "p AND q OR r" to be valid', () => {
+    expect(isValid('p AND q OR r')).toBe(true);
+  });
+
+  it('evaluates "p q" to be invalid', () => {
+    expect(isValid('p q')).toBe(false);
+  });
+
+  it('evaluates "p AND q OR" to be invalid', () => {
+    expect(isValid('p AND q OR')).toBe(false);
+  });
+});
+
+describe('expressions with parenthesis', () => {
+  it('evaluates "(p AND q) OR r" to be valid', () => {
+    expect(isValid('(p AND q) OR r')).toBe(true);
+  });
+});
