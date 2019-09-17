a35f7221f1c3f0a52b39c9924f55f449e0bd68fb
Daniel Gil <dangil@microsoft.com>
2019-09-16 18:40:03 -0700

    Comments on isValid

--- src/parser/Parser.ts
+++ src/parser/Parser.ts
@@ -1,3 +1,9 @@
+/**
+ * Initial pass through an expression to determine if it is valid.
+ * Ensures there are no invalid tokens, all parenthesis are closed,
+ * no consecutive special keywords and no consecutive variables.
+ * @param expression
+ */
 export function isValid(expression: string): boolean {
   return false;
 }
