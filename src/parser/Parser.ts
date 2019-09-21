3f999e5f4ea66422fae4180450688a287be60832
Daniel Gil <daniel.patrick.gil@gmail.com>
2019-09-21 11:13:49 -0700

    Move comment to ParserAssertion

--- src/parser/Parser.ts
+++ src/parser/Parser.ts
@@ -1,11 +1,5 @@
 import { assertIsValidExpression } from './ParserAssertions';
 
-/**
- * Initial pass through an expression to determine if it is valid.
- * Ensures there are no invalid tokens, all parenthesis are closed,
- * no consecutive special keywords and no consecutive variables.
- * @param expression
- */
 export function parse(expression: string): void {
   assertIsValidExpression(expression);
 }
--- src/parser/ParserAssertions.ts
+++ src/parser/ParserAssertions.ts
@@ -2,6 +2,12 @@ const validTokens = new Set(
   'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ() '.split('')
 );
 
+/**
+ * Initial pass through an expression to determine if it is valid.
+ * Ensures there are no invalid tokens, all parenthesis are closed,
+ * no consecutive special keywords and no consecutive variables.
+ * @param expression
+ */
 export function assertIsValidExpression(expression: string): void {
   assertNoInvalidTokens(expression);
   assertMatchesParenthesis(expression);
