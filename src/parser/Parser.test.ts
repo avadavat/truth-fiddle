3f7e6c05a6d552ef57c2514bf734eab9fb395af7
Daniel Gil <daniel.patrick.gil@gmail.com>
2019-09-21 11:59:47 -0700

    Ensure no duplicates in extracted variable list

--- src/parser/Parser.test.ts
+++ src/parser/Parser.test.ts
@@ -1,15 +1,23 @@
 import { parse } from './Parser';
 
 describe('variable extraction tests', () => {
-  it('extracts [p, q] from "p AND q"', () => {
-    expect(parse('p AND q')).toEqual(['p', 'q']);
+  const exp1 = 'p AND q';
+  it(`extracts [p, q] from "${exp1}"`, () => {
+    expect(parse(exp1)).toEqual(['p', 'q']);
   });
 
-  it('extracts [p, q, r] from "p AND q OR r"', () => {
-    expect(parse('p AND q OR r')).toEqual(['p', 'q', 'r']);
+  const exp2 = 'p AND q OR r';
+  it(`extracts [p, q, r] from "${exp2}"`, () => {
+    expect(parse(exp2)).toEqual(['p', 'q', 'r']);
   });
 
-  it('extracts [p, q, r] from " ( p AND (q OR r))"', () => {
-    expect(parse('p AND q OR r')).toEqual(['p', 'q', 'r']);
+  const exp3 = ' ( p AND (q OR r))';
+  it(`extracts [p, q, r] from "${exp3}"`, () => {
+    expect(parse(exp3)).toEqual(['p', 'q', 'r']);
+  });
+
+  const exp4 = ' ( p AND (q OR r) AND p OR q)';
+  it(`extracts [p, q, r] from "${exp4}"`, () => {
+    expect(parse(exp4)).toEqual(['p', 'q', 'r']);
   });
 });
--- src/parser/Parser.ts
+++ src/parser/Parser.ts
@@ -7,11 +7,21 @@ const splitRegex = new RegExp(/[ ()]+/g);
 
 export function parse(expression: string): string[] {
   assertIsValidInput(expression);
-  const words = expression.split(splitRegex);
+  return extractVariables(expression);
+}
+
+function extractVariables(expression: string): string[] {
+  const words: string[] = expression.split(splitRegex);
 
   // Any word that isn't a reserved keyword is a variable.
-  const variables = words.filter(
-    word => !reservedKeywords.has(word.toLowerCase())
+  const variables: string[] = words.filter(
+    word => word.length > 0 && !reservedKeywords.has(word.toLowerCase())
   );
-  return variables;
+
+  // Only count each variable once.
+  const uniqueVariables: string[] = variables.filter(
+    (variable, i) => variables.indexOf(variable) === i
+  );
+
+  return uniqueVariables;
 }
