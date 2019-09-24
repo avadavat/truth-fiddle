c0fdcaedbb59dcd1e4b4775b2d7de7f8e15fed5d
Daniel Gil <daniel.patrick.gil@gmail.com>
2019-09-23 22:15:15 -0700

    Allow query through url

--- src/view/AppBody.tsx
+++ src/view/AppBody.tsx
@@ -1,25 +1,37 @@
 import React from 'react';
+import QueryString from 'query-string';
 import { InputBox } from './InputBox';
 import { TruthTable } from './truthTable';
 import { evaluate, parse, ParseResult, QueryPermutation } from '../model';
 
 // Expression the user sees when initially loading the application.
-const initialExpression = 'p and q';
+const defaultExpression = 'p and q';
+
+const getInitialQuery = (): string => {
+  // Set the query to the query specified by the url or the default.
+  const params = QueryString.parseUrl(window.location.search);
+  const initialQuery: string = params.query.q
+    ? params.query.q.toString()
+    : defaultExpression;
+  return initialQuery;
+};
 
 /**
  * AppBody contains everything in the application, just used
  * as a layer of abstraction from App.
  */
 export const AppBody = React.memo(function AppBody() {
-  const [query, setQuery] = React.useState<string>(initialExpression);
-  const [result, setResult] = React.useState<QueryPermutation[]>(
-    evaluate(parse(initialExpression))
-  );
+  const [query, setQuery] = React.useState<string>('');
+  const [result, setResult] = React.useState<QueryPermutation[]>([]);
   const [message, setMessage] = React.useState<string>('');
 
   const onQueryChange = (newQuery: string) => {
     setQuery(newQuery);
   };
+
+  React.useEffect(() => {
+    setQuery(getInitialQuery());
+  }, []);
 
   React.useEffect(() => {
     try {
