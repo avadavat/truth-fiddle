5aa5a9ed0c28877131ffe14040ef824bbe0dcd98
Daniel Gil <daniel.patrick.gil@gmail.com>
2020-02-10 22:42:14 -0500

    Make links open in new tab

--- src/view/SyntaxTable.tsx
+++ src/view/SyntaxTable.tsx
@@ -11,13 +11,18 @@ export const SyntaxTable = React.memo(function SyntaxTable() {
           </tr>
           <tr>
             <td>
-              <a href="https://en.wikipedia.org/wiki/Negation">Negation</a>
+              <a href="https://en.wikipedia.org/wiki/Negation" target="_blank">
+                Negation
+              </a>
             </td>
             <td>!</td>
           </tr>
           <tr>
             <td>
-              <a href="https://en.wikipedia.org/wiki/Logical_conjunction">
+              <a
+                href="https://en.wikipedia.org/wiki/Logical_conjunction"
+                target="_blank"
+              >
                 Conjuction
               </a>
             </td>
@@ -25,7 +30,10 @@ export const SyntaxTable = React.memo(function SyntaxTable() {
           </tr>
           <tr>
             <td>
-              <a href="https://en.wikipedia.org/wiki/Logical_disjunction">
+              <a
+                href="https://en.wikipedia.org/wiki/Logical_disjunction"
+                target="_blank"
+              >
                 Inclusive Disjunction
               </a>
             </td>
@@ -33,7 +41,10 @@ export const SyntaxTable = React.memo(function SyntaxTable() {
           </tr>
           <tr>
             <td>
-              <a href="https://en.wikipedia.org/wiki/Exclusive_or">
+              <a
+                href="https://en.wikipedia.org/wiki/Exclusive_or"
+                target="_blank"
+              >
                 Exclusive Disjunction
               </a>
             </td>
@@ -41,7 +52,10 @@ export const SyntaxTable = React.memo(function SyntaxTable() {
           </tr>
           <tr>
             <td>
-              <a href="https://en.wikipedia.org/wiki/Material_conditional">
+              <a
+                href="https://en.wikipedia.org/wiki/Material_conditional"
+                target="_blank"
+              >
                 Implication
               </a>
             </td>
@@ -49,7 +63,10 @@ export const SyntaxTable = React.memo(function SyntaxTable() {
           </tr>
           <tr>
             <td>
-              <a href="https://en.wikipedia.org/wiki/Converse_implication">
+              <a
+                href="https://en.wikipedia.org/wiki/Converse_implication"
+                target="_blank"
+              >
                 Converse Implication
               </a>
             </td>
@@ -57,7 +74,10 @@ export const SyntaxTable = React.memo(function SyntaxTable() {
           </tr>
           <tr>
             <td>
-              <a href="https://en.wikipedia.org/wiki/If_and_only_if">
+              <a
+                href="https://en.wikipedia.org/wiki/If_and_only_if"
+                target="_blank"
+              >
                 Biconditional
               </a>
             </td>
