8d8d8fbf0775298ea29437ffdbb4619d1979869c
Daniel Gil <7670571+dpgil@users.noreply.github.com>
2020-02-10 22:49:58 -0500

    Current logo 2 big (#45)
    * Add 64px github logo
    
    * Switch to the 64px logo

--- src/view/Footer.tsx
+++ src/view/Footer.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 export const Footer = React.memo(function Footer() {
   return (
-    <div className="Footer" >
-        <a href="https://github.com/avadavat/truth-fiddle">
-        	<img src="/img/github-logo-light.png" alt="Github Logo" title="Source Code" />
-        </a>
+    <div className="Footer">
+      <a href="https://github.com/avadavat/truth-fiddle">
+        <img
+          src="/img/github-logo-light-64px.png"
+          alt="Github Logo"
+          title="Source Code"
+        />
+      </a>
     </div>
   );
 });
