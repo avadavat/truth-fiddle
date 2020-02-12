0c7dec08386f1183b396fad75cfd2340b04a05d8
Daniel Gil <daniel.patrick.gil@gmail.com>
2020-02-11 23:40:41 -0500

    Open github link in new tab

--- src/view/Footer.tsx
+++ src/view/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Footer = React.memo(function Footer() {
   return (
     <div className="Footer">
-      <a href="https://github.com/avadavat/truth-fiddle">
+      <a href="https://github.com/avadavat/truth-fiddle" target="_blank">
         <img
           src="/img/github-logo-light-64px.png"
           alt="Github Logo"
