195eede62aefbd4032859c832e9506499d345528
Joe Badlato <joebadlato@gmail.com>
2020-02-11 22:16:33 -0500

    Simplify variable extracter - only consider strings that have only alphanumeric characters

--- src/model/parser/extractVariables.ts
+++ src/model/parser/extractVariables.ts
@@ -1,9 +1,7 @@
 // Splits by parentheses, white space, and special characters.
 const splitRegex = new RegExp(/[ ()|&~!^]+/g);
-const reservedKeywords = new Set(['and', 'not', 'or', 'xor', '&&', '&', '||',
- '|', '~', '!', '^', '->', 'if', 'then', '<->', '=', '==', 'iff', 'only', '<-',
- 'true', 'false', '\\/', '/\\', '<-->', '<==>', '<=>', '-->', '==>', '=>', '<=',
- '<==', '<--']);
+const reservedKeywords = new Set(['and', 'not', 'or', 'xor', 'if', 'then',
+  'iff', 'only','true', 'false']);
 
 /**
  * Returns a list of all the unique variable names in the given expression.
@@ -12,8 +10,15 @@ export function extractVariables(expression: string): string[] {
   const words: string[] = expression.split(splitRegex);
 
   // Any word that isn't a reserved keyword is a variable.
-  const variables: string[] = words.filter(
-    word => word.length > 0 && !reservedKeywords.has(word.toLowerCase())
+  const variables: string[] = words
+  .filter(
+    word => word.length > 0
+  )
+  .filter(
+    word => /^[A-Za-z0-9]+$/.test(word)
+  )
+  .filter(
+    word => !reservedKeywords.has(word.toLowerCase())
   );
 
   // Only count each variable once.
